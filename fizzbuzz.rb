c3f034f1bbcec7f7638e23a3ff3411f3a437d803
Sol Jin <soljin01@gmail.com>
2016-07-25 03:08:27 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizzbuzz(int)
+  if int % 15 == 0
+    puts fizzbuzz = "FizzBuzz"
+    fizzbuzz
+  elsif int % 5 == 0
+    puts buzz = "Buzz"
+    buzz
+  elsif int % 3 == 0
+    puts fizz = "Fizz"
+    fizz
+  else
+    puts "nil"
+  end
+end
